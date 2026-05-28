89cc266e58cc8b460a57d8ac00b9fe743d69459b
dendev <dendev.mobile@gmail.com>
2026-05-28 14:59:36 +0200

    dynamix port or 3000ù

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 const host = "http://localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Running on ${host}:${port}`);
